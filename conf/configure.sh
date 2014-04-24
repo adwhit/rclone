4df789d4fe33019d5f2f58a416fca3b1b422b4ee
Alex Whitney <aw1209@ic.ac.uk>
2014-04-24 18:07:19 +0000

    conf tweaking, now working

--- conf/configure.sh
+++ conf/configure.sh
@@ -1,4 +1,6 @@
-ROOT="/home/$USER/proj/rclone"
+ROOT="$HOME/proj/rclone"
+
+sudo rm /etc/nginx/sites-enabled/nginx.conf
 sudo ln -s $ROOT/conf/nginx.conf /etc/nginx/sites-enabled/
 mkdir logs
 touch $ROOT/logs/access.log
